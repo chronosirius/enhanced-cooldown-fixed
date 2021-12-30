9d27e5f17e2199e3cf642925c7a8d526f8d5e49b
Toricane <73972068+Toricane@users.noreply.github.com>
2021-12-29 20:35:54 -0800

    chore: version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-better-components",
-    version="1.1.1",
+    version="1.2.0",
     description="Better components for discord-py-interactions",
     long_description=long_description,
     long_description_content_type="text/markdown",
