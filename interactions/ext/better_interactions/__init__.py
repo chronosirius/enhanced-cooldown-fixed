4188bf0f9c24549e6b49e773ae2b291da1ce7156
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-02 16:34:16 -0800

    fix import

--- interactions/ext/better_interactions/__init__.py
+++ interactions/ext/better_interactions/__init__.py
@@ -1,6 +1,6 @@
 from . import *
 
 from .callback import component
-from .subcomand import subcommand
+from .subcomand import base
 from .components import ActionRow, Button, SelectMenu, spread_to_rows
 from .setup import setup
