b3234f089eb0a21e26861451ed0557ce3ec5966b
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-12 09:52:37 -0800

    aaaa

--- interactions/ext/better_interactions/extension.py
+++ interactions/ext/better_interactions/extension.py
@@ -36,57 +36,56 @@ class ExtendedWebSocket(interactions.api.gateway.WebSocket):
 interactions.api.gateway.WebSocket = ExtendedWebSocket
 
 
+def sync_subcommands(self):
+    print("original init")
+    client = self.client
+    print("E", getmembers(self, predicate=iscoroutinefunction))
+    # get the methods
+    if any(
+        hasattr(func, "__subcommand__")
+        for _, func in getmembers(self, predicate=iscoroutinefunction)
+    ):
+        bases = {
+            func.__base__: func.__data__
+            for _, func in getmembers(self, predicate=iscoroutinefunction)
+            if hasattr(func, "__subcommand__")
+        }
+        print(bases)
+        commands = []
+
+        for subcommand in bases.values():
+            client.event(subcommand.inner, name=f"command_{subcommand.base}")
+            commands.extend(subcommand.raw_commands)
+
+        if client._automate_sync:
+
+            if client._loop.is_running():
+                [
+                    client._loop.create_task(client._synchronize(command))
+                    for command in commands
+                ]
+            else:
+                [
+                    client._loop.run_until_complete(client._synchronize(command))
+                    for command in commands
+                ]
+        for subcommand in bases.values():
+            scope = subcommand.scope
+            if scope is not None:
+                if isinstance(scope, list):
+                    [
+                        client._scopes.add(_ if isinstance(_, int) else _.id)
+                        for _ in scope
+                    ]
+                else:
+                    client._scopes.add(scope if isinstance(scope, int) else scope.id)
+
+
 class BetterExtension(interactions.Extension):
     def __new__(cls, client, *args, **kwargs):
         self = super().__new__(cls, client, *args, **kwargs)
-        self.sync_subcommands()
+        sync_subcommands(self)
         return self
-
-    def sync_subcommands(self):
-        print("original init")
-        client = self.client
-        print("E", getmembers(self, predicate=iscoroutinefunction))
-        # get the methods
-        if any(
-            hasattr(func, "__subcommand__")
-            for _, func in getmembers(self, predicate=iscoroutinefunction)
-        ):
-            bases = {
-                func.__base__: func.__data__
-                for _, func in getmembers(self, predicate=iscoroutinefunction)
-                if hasattr(func, "__subcommand__")
-            }
-            print(bases)
-            commands = []
-
-            for subcommand in bases.values():
-                client.event(subcommand.inner, name=f"command_{subcommand.base}")
-                commands.extend(subcommand.raw_commands)
-
-            if client._automate_sync:
-
-                if client._loop.is_running():
-                    [
-                        client._loop.create_task(client._synchronize(command))
-                        for command in commands
-                    ]
-                else:
-                    [
-                        client._loop.run_until_complete(client._synchronize(command))
-                        for command in commands
-                    ]
-            for subcommand in bases.values():
-                scope = subcommand.scope
-                if scope is not None:
-                    if isinstance(scope, list):
-                        [
-                            client._scopes.add(_ if isinstance(_, int) else _.id)
-                            for _ in scope
-                        ]
-                    else:
-                        client._scopes.add(
-                            scope if isinstance(scope, int) else scope.id
-                        )
 
 
 class Extension(interactions.client.Extension):
