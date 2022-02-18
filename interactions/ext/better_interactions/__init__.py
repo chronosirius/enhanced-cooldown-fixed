24c28d194d20892def1641ef283187e4206d4686
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-18 12:06:31 -0800

    change

--- interactions/ext/better_interactions/__init__.py
+++ interactions/ext/better_interactions/__init__.py
@@ -3,7 +3,7 @@ from interactions.base import __version__ as __lib_version__
 __version__ = "2.1.2"
 __ext_version__ = f"{__lib_version__}:{__version__}"
 
-from . import *
+from . import callback, subcomand, command, command_models, components, extension
 
 from .callback import *
 from .subcomand import *
