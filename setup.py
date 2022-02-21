58f546c4a9394d820a3fc7244fa2dc9616a2babf
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-21 13:54:30 -0800

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from re import MULTILINE, search
-
-from setuptools import setup
-from torch import long
-
-with open("interactions/ext/better_interactions/__init__.py") as f:
-    version = search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), MULTILINE).group(1)
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-
 from interactions.ext import Base, Version, VersionAuthor, build
 
 # gets the long description from the README file
