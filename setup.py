bc77a78a604a1612d8b04595b10279e1178dbacd
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-21 15:03:22 -0800

    fix: thank you catalyst4

--- setup.py
+++ setup.py
@@ -1,40 +1,36 @@
-# # from interactions.ext import Base, Version, VersionAuthor, build
-# from .interactions.ext.base import Base, Version, VersionAuthor, build
+from interactions.ext import Base, Version, VersionAuthor, build
 
-# # gets the long description from the README file
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+# gets the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-# # data for pypi:
-# version = Version(
-#     major=3,
-#     minor=0,
-#     patch=0,
-#     author=VersionAuthor(name="Toricane", email="prjwl028@gmail.com"),
-# )
-
-# data = {
-#     "name": "better_interactions",
-#     "description": "Better interactions for interactions.py",
-#     "long_description": long_description,
-#     "version": version,
-#     "link": "https://github.com/Toricane/better-interactions",
-#     "dependencies": ["interactions.ext.better_interactions"],
-#     "requirements": ["discord-py-interactions>=4.1.0"],
-# }
-
-# build(Base(**data))
-from _setup import base
-from setuptools import setup
-
-setup(
-    name=base.name,
-    version=str(base.version),
-    description=base.description,
-    long_description="" if base.long_description is None else base.long_description,
-    author=base.version.author,
-    author_email=base.version.author.email,
-    url=base.link,
-    packages=[] if base._dependencies is None else base._dependencies,
-    install_requires=[] if base._requirements is None else base._requirements,
+# data for pypi:
+version = Version(
+    major=3,
+    minor=0,
+    patch=0,
+    author=VersionAuthor(name="Toricane", email="prjwl028@gmail.com"),
 )
+
+data = {
+    "name": "better_interactions",
+    "description": "Better interactions for interactions.py",
+    "long_description": long_description,
+    "version": version,
+    "link": "https://github.com/Toricane/better-interactions",
+    "dependencies": ["interactions.ext.better_interactions"],
+    "requirements": ["discord-py-interactions>=4.1.0"],
+    "setup_requires": ["discord-py-interactions"],
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    "license": "MIT",
+    "author": "Toricane",
+    "author_email": "prjwl028@gmail.com",
+    "long_description_content_type": "text/markdown",
+}
+
+build(Base(**data))
