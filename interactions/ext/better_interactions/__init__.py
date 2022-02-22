68d444a5c24b954fd0d47b2b693c834c7308ceff
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-21 20:06:26 -0800

    refactor: bruh

--- interactions/ext/better_interactions/__init__.py
+++ interactions/ext/better_interactions/__init__.py
@@ -1,19 +1,19 @@
 # flake8: noqa
-# from . import _logging, extension, cmd, cmpt
-# from ._logging import CustomFormatter, Data, get_logger
-# from .cmd import command_models, commands, subcommands
-# from .cmd.command_models import BetterOption
-# from .cmd.commands import autodefer, command, extension_command
-# from .cmd.subcommands import base, extension_base
-# from .cmpt import callback, components
-# from .cmpt.callback import component
-# from .cmpt.components import ActionRow, Button, SelectMenu, spread_to_rows
-# from .extension import (
-#     BetterExtension,
-#     BetterInteractions,
-#     setup,
-#     sync_subcommands,
-# )
+from . import _logging, extension  # , cmd, cmpt
+from ._logging import CustomFormatter, Data, get_logger
+from .cmd import command_models, commands, subcommands
+from .cmd.command_models import BetterOption
+from .cmd.commands import autodefer, command, extension_command
+from .cmd.subcommands import base, extension_base
+from .cmpt import callback, components
+from .cmpt.callback import component
+from .cmpt.components import ActionRow, Button, SelectMenu, spread_to_rows
+from .extension import (
+    BetterExtension,
+    BetterInteractions,
+    setup,
+    sync_subcommands,
+)
 
 # fmt: off
 __all__ = [
