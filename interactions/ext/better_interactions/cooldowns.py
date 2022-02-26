e16370b4023ffedc681b65835e25efb31f765f34
Toricane <73972068+Toricane@users.noreply.github.com>
2022-02-25 20:30:25 -0800

    docs: document cooldowns

--- interactions/ext/better_interactions/cooldowns.py
+++ interactions/ext/better_interactions/cooldowns.py
@@ -10,125 +10,18 @@ from interactions import Channel, CommandContext, Guild, Member, User
 
 NoneType: Type[None] = type(None)
 
-"""
-class cooldown:
-    def __init__(
-        self,
-        function: Optional[Coroutine] = None,
-        cal: Optional[Coroutine] = None,
-        cool: Optional[Union[float, int]] = 10,
-        typ: Optional[Union[str, User, Channel, Guild]] = "user",
-    ):
-        if typ not in {"user", User, "guild", Guild, "channel", Channel}:
-            raise TypeError("Invalid type provided for `typ`!")
-        self.function = function
-        self.js = {}
-        self.cool = cool
-        self.cal = cal
-        self.typ = typ
 
-    def _clean_timers(self):
-        jsondata = [obj for obj in self.js if time() - self.js[obj] >= self.cool]
-        self.js = jsondata
+def get_id(
+    type: Optional[Union[str, User, Channel, Guild]], ctx: CommandContext
+) -> str:
+    """
+    Returns the appropriate ID for the type provided.
 
-    def data(self, ctx):
-        typ = self.typ
-        js = self.js
-
-        if typ in {"user", User}:
-            id = str(ctx.author.user.id)
-        elif typ in {"channel", Channel}:
-            id = str(ctx.channel.id)
-        elif typ in {"guild", Guild}:
-            id = str(ctx.guild.id)
-        else:
-            raise TypeError(f"Invalid type: {typ}")
-
-        data = js.get(id, time())
-        if not js.get(id, None):
-            js[id] = data
-            return (True, data)
-        if time() - data < self.cool:
-            return (False, data)
-        data = time()
-        return (True, data)
-
-    def __call__(self, func):
-        @wraps(func)
-        async def new_func(ctx: CommandContext, *args, **kwargs):
-            data = self.data(ctx)
-            if data[0]:
-                return await func(ctx, *args, **kwargs)
-            if self.cal:
-                return await self.function(ctx, self.cool - (time() - data[1]))
-            await ctx.send("This command is currently on cooldown")
-            new_data = filter(lambda attr: attr not in dir(type(func)), dir(func))
-            for new_attr in new_data:
-                old_attr = getattr(func, new_attr)
-                setattr(new_func, new_attr, old_attr)
-
-        return new_func
-"""
-
-"""
-class cooldown:
-    def __init__(
-        self,
-        time: Optional[Union[float, int]] = 10,
-        cooldown_function: Optional[Coroutine] = None,
-        type: Optional[Union[str, User, Channel, Guild]] = "user",
-    ):
-        if type not in {"user", User, "guild", Guild, "channel", Channel}:
-            raise TypeError("Invalid type provided for `type`!")
-
-        self.time = time
-        self.cooldown_function = cooldown_function
-        self.type = type
-
-        self.func = None
-        self.json = {}
-
-    def reset_cooldown(self):
-        self.json = [obj for obj in self.json if _time() - self.json[obj] >= self.time]
-
-    def cooldown_passed(self, ctx):
-        if self.type in {"user", User}:
-            id = str(ctx.author.user.id)
-        elif self.type in {"channel", Channel}:
-            id = str(ctx.channel.id)
-        elif self.type in {"guild", Guild}:
-            id = str(ctx.guild.id)
-        else:
-            raise TypeError(f"Invalid type: {self.type}")
-
-        data = self.json.get(id, _time())
-        if not self.json.get(id, None):
-            self.json[id] = data
-            return True, data
-        if _time() - data < self.time:
-            return False, data
-        return True, data
-
-    def __call__(self, func):
-        @wraps(func)
-        async def new_func(ctx: CommandContext, *args, **kwargs):
-            cooled, data = self.cooldown_passed(ctx)
-            if cooled:
-                return await func(ctx, *args, **kwargs)
-            if self.cooldown_function:
-                return await self.cooldown_function(ctx, self.time - (_time() - data))
-            await ctx.send("This command is currently on cooldown!")
-            # new_data = filter(lambda attr: attr not in dir(type(func)), dir(func))
-            # for new_attr in new_data:
-            #     old_attr = getattr(func, new_attr)
-            #     setattr(new_func, new_attr, old_attr)
-
-        self.func = new_func
-        return new_func
-"""
-
-
-def get_id(type, ctx):
+    :param type: The type of ID to get.
+    :type type: Union[str, User, Channel, Guild]
+    :param ctx: The context to get the ID from.
+    :type ctx: CommandContext
+    """
     if type == "user" or type is User or type == "member" or type is Member:
         return str(ctx.author.user.id)
     elif type == "channel" or type is Channel:
@@ -145,6 +38,18 @@ def cooldown(
     type: Optional[Union[str, User, Channel, Guild]] = "user",
     **delta_kwargs,
 ):
+    """
+    A decorator for handling cooldowns.
+
+    :param delta_args: The arguments to pass to `datetime.timedelta`.
+    :type delta_args: Tuple[Any]
+    :param error: The error function to call if the command is on cooldown.
+    :type error: Coroutine
+    :param type: The type of ID to utilize for the cooldown.
+    :type type: Union[str, User, Channel, Guild]
+    :param delta_kwargs: The keyword arguments to pass to `datetime.timedelta`.
+    :type delta_kwargs: Dict[str, Any]
+    """
     if not delta_args or delta_kwargs:
         raise ValueError(
             "Cooldown amount must be provided! Valid arguments and keyword arguments are listed in https://docs.python.org/3/library/datetime.html#datetime.timedelta"
