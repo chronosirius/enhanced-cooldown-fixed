547bb6a65086598c0c2af286386b9b801ab768fc
Toricane <73972068+Toricane@users.noreply.github.com>
2022-03-20 20:58:51 -0700

    chore: version up 3.2.1

--- interactions/ext/enhanced/extension.py
+++ interactions/ext/enhanced/extension.py
@@ -32,7 +32,7 @@ class BasePatch(Base):
 
 version = (
     VersionPatch(
-        version="3.2.0",
+        version="3.2.1",
         author=VersionAuthorPatch(
             name="Toricane",
             email="prjwl028@gmail.com",
