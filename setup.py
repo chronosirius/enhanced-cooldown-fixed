598951700d8abd21ecc7a825d1e8f6d077f1a7a3
Toricane <73972068+Toricane@users.noreply.github.com>
2022-10-13 17:22:20 -0700

    chore: version up 4.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="enhanced",
-    version="4.0.0",
+    version="4.0.1",
     description="Enhanced interactions for interactions.py",
     long_description=long_description,
     long_description_content_type="text/markdown",
